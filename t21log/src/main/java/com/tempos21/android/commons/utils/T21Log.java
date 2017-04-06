b6e821b6d1ac7853ba06ad0240f97a2f3d4e9d37
Jacobo Conejo Lima <jacobo.conejolima@worldline.com>
2017-04-06 18:46:03 +0200

    Added FileLogger to T21Log.
    This way, T21Log can be configured to write log to a file.

--- t21log/src/main/java/com/tempos21/android/commons/utils/T21Log.java
+++ t21log/src/main/java/com/tempos21/android/commons/utils/T21Log.java
@@ -1,5 +1,7 @@
 package com.tempos21.android.commons.utils;
 
+import java.io.File;
+
 /**
  * Custom Log class
  */
@@ -28,6 +30,18 @@ public class T21Log {
         T21Log.LOG_ENABLED = enabled;
     }
 
+    /**
+     * Initialize the Log to write both to Android logger and file
+     * @param tag:     Your appName or something You want to appear in log;
+     * @param enabled: If you want to enable or disable the log
+     * @param logFile: File where log will be written to
+     */
+    public static void initialize(String tag, boolean enabled, File logFile) {
+        T21Log.logTag = tag;
+        T21Log.LOG_ENABLED = enabled;
+        FileLogger.initialize(logFile);
+    }
+
     private static String getString(Object object) {
         String string = null;
         try {
@@ -46,12 +60,14 @@ public class T21Log {
     public static void v(Object... verbose) {
         if (LOG_ENABLED) {
             android.util.Log.v(getLogTag(), getLog(verbose));
+            FileLogger.v(getLogTag(), getLog(verbose));
         }
     }
 
     public static void d(Object... debug) {
         if (LOG_ENABLED) {
             android.util.Log.d(getLogTag(), getLog(debug));
+            FileLogger.d(getLogTag(), getLog(debug));
         }
     }
 
@@ -59,18 +75,21 @@ public class T21Log {
     public static void i(Object... info) {
         if (LOG_ENABLED) {
             android.util.Log.i(getLogTag(), getLog(info));
+            FileLogger.i(getLogTag(), getLog(info));
         }
     }
 
     public static void w(Object... warning) {
         if (LOG_ENABLED) {
             android.util.Log.w(getLogTag(), getLog(warning));
+            FileLogger.w(getLogTag(), getLog(warning));
         }
     }
 
     public static void e(Object... error) {
         if (LOG_ENABLED) {
             android.util.Log.e(getLogTag(), getLog(error));
+            FileLogger.e(getLogTag(), getLog(error));
         }
     }
 
