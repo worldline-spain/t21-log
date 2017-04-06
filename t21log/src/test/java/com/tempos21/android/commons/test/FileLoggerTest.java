c61fa9ed048108a259c96c132fef295f61b53d6f
Jacobo Conejo Lima <jacobo.conejolima@worldline.com>
2017-04-06 18:43:29 +0200

    Created tests for FileLogger class.

---
+++ t21log/src/test/java/com/tempos21/android/commons/test/FileLoggerTest.java
@@ -0,0 +1,193 @@
+package com.tempos21.android.commons.test;
+
+import com.tempos21.android.commons.utils.FileLogger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Log.class})
+public class FileLoggerTest {
+
+    private static File logFile;
+
+    private static String TAG = "[FileLoggerTest]";
+
+    @BeforeClass
+    public static void setUpOnce() {
+        logFile = new File(/*System.getProperty("user.home"), */"FileLoggerTest.log");
+        FileLogger.initialize(logFile);
+    }
+
+    @Test
+    public void verbose() {
+        String VERBOSE_TAG = "V";
+
+        BuildingTest buildingTest = new BuildingTest();
+        buildingTest.setId(2);
+        buildingTest.setName("nameTestVerbose");
+        buildingTest.setAddress("addressTestVerbose");
+        FileLogger.v(TAG, buildingTest.toString());
+
+        checkLogFile(VERBOSE_TAG, buildingTest);
+    }
+
+    @Test
+    public void debug() {
+        String DEBUG_TAG = "D";
+
+        BuildingTest buildingTest = new BuildingTest();
+        buildingTest.setId(0);
+        buildingTest.setName("nameTestDebug");
+        buildingTest.setAddress("addressTestDebug");
+        FileLogger.d(TAG, buildingTest.toString());
+
+        checkLogFile(DEBUG_TAG, buildingTest);
+    }
+
+    @Test
+    public void info() {
+        String INFO_TAG = "I";
+
+        BuildingTest buildingTest = new BuildingTest();
+        buildingTest.setId(3);
+        buildingTest.setName("nameTestInfo");
+        buildingTest.setAddress("addressTestInfo");
+        FileLogger.d(TAG, buildingTest.toString());
+
+        checkLogFile(INFO_TAG, buildingTest);
+    }
+
+    @Test
+    public void warning() {
+        String WARNING_TAG = "W";
+
+        BuildingTest buildingTest = new BuildingTest();
+        buildingTest.setId(12);
+        buildingTest.setName("nameTestWarning");
+        buildingTest.setAddress("addressTestWarning");
+        FileLogger.d(TAG, buildingTest.toString());
+
+        checkLogFile(WARNING_TAG, buildingTest);
+    }
+
+    @Test
+    public void error() {
+        String ERROR_TAG = "E";
+
+        BuildingTest buildingTest = new BuildingTest();
+        buildingTest.setId(99);
+        buildingTest.setName("nameTestError");
+        buildingTest.setAddress("addressTestError");
+        FileLogger.d(TAG, buildingTest.toString());
+
+        checkLogFile(ERROR_TAG, buildingTest);
+    }
+
+    private static void checkLogFile(String logLevelTag, BuildingTest buildingTest) {
+        String lineLogged = readFileLastLine();
+
+        assertTrue("Verbose tag '" + logLevelTag + "' not found in log file", lineLogged.contains(logLevelTag));
+        assertTrue("Log tag '" + TAG + "' not found in log file", lineLogged.contains(TAG));
+
+        String dateTimeString = lineLogged.substring(0, lineLogged.indexOf(TAG) - 2);
+        DateFormat formatter = SimpleDateFormat.getDateTimeInstance();
+        try {
+            formatter.parse(dateTimeString);
+        } catch (ParseException e) {
+            fail("Date and time not found in log file");
+        }
+
+        assertTrue("Object logged '" + buildingTest + "' not found in log file", lineLogged.contains(buildingTest.toString()));
+    }
+
+    private static String readFileLastLine() {
+        String line = "";
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
+
+        try {
+            String currentLine;
+
+            bufferedReader = new BufferedReader(new FileReader(logFile));
+
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                line = currentLine;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return line;
+    }
+
+    @AfterClass
+    public static void tearDownOnce() {
+        logFile.delete();
+    }
+
+    class BuildingTest {
+        private int id;
+
+        private String name;
+
+        private String address;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return "BuildingTest{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+    }
+}
