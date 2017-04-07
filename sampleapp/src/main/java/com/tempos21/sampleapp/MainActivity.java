9837749960f60423624cd9a402ebf9f053253b3a
Jacobo Conejo Lima <jacobo.conejolima@worldline.com>
2017-04-07 14:53:46 +0200

    Added button to SampleApp to be able to share log file by email.

--- sampleapp/src/main/java/com/tempos21/sampleapp/MainActivity.java
+++ sampleapp/src/main/java/com/tempos21/sampleapp/MainActivity.java
@@ -2,18 +2,42 @@ package com.tempos21.sampleapp;
 
 import com.tempos21.android.commons.utils.T21Log;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "[MainActivity]";
+
+    private Button sendByMailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sendByMailButton = (Button) findViewById(R.id.sendByMailButton);
+        sendByMailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File logFile = ((SampleApplication) getApplication()).getLogFile();
+                Uri path = Uri.fromFile(logFile);
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                // set the type to 'email'
+                emailIntent.setType("message/rfc822");
+                // the attachment
+                emailIntent.putExtra(Intent.EXTRA_STREAM, path);
+                // the mail subject
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject));
+                startActivity(Intent.createChooser(emailIntent, getString(R.string.mail_chooser_title)));
+            }
+        });
         T21Log.d(TAG, "onCreate", "adding messages", 3, true, "separated by commas");
 
     }
--- sampleapp/src/main/java/com/tempos21/sampleapp/SampleApplication.java
+++ sampleapp/src/main/java/com/tempos21/sampleapp/SampleApplication.java
@@ -4,13 +4,24 @@ import com.tempos21.android.commons.utils.T21Log;
 
 import android.app.Application;
 
+import java.io.File;
+
 public class SampleApplication extends Application {
 
     private static final String LOG_TAG = "[SAMPLE_APP]";
 
+    private static final String LOG_FILE_NAME = "SampleApp.log";
+
+    private File logFile;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        T21Log.initialize(LOG_TAG, BuildConfig.DEBUG);
+        logFile = new File(getFilesDir(), LOG_FILE_NAME);
+        T21Log.initialize(LOG_TAG, BuildConfig.DEBUG, logFile);
+    }
+
+    public File getLogFile() {
+        return logFile;
     }
 }
